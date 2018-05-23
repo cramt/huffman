e9987181f7e01cc6631db1e4600b7feb792ab1ce
mads østermark <madscramt@gmail.com>
2018-05-23 13:18:32 +0200

    Add project files.

---
+++ huffman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace huffman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
